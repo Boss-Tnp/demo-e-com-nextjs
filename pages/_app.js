77125b15e9e160168940ae1ab3989b702a1b8030
Tanapon Satusinprasert <tanapon.satu@gmail.com>
2020-08-07 18:59:41 +0700

    test deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,41 @@
 import Layout from "./layout/layout";
 import { useStore } from "./../store/store";
 import { Provider } from "react-redux";
+import theme from "../src/theme";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+import { useEffect } from "react";
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
 
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <React.Fragment>
+      <Head>
+        <title>Demo E-commerce</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </Provider>
+    </React.Fragment>
   );
 }
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,54 @@
+import Axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import MyHome from "../components/home/home";
+import * as actions from "../store/action/index";
+import { API_HEADER, GRAPHQLAPI_ENDPOINT } from "../utils/constant";
+
 export default function Home(props) {
-  return <p>Hello World</p>;
+  // console.log(props);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(actions.setProducts(props.products));
+  }, [dispatch]);
+  return <MyHome />;
+}
+
+export async function getStaticProps() {
+  const res = await Axios.post(
+    GRAPHQLAPI_ENDPOINT,
+    {
+      query: `
+          query {
+            getProducts {
+              _id
+              info {
+                brand model description stock
+              }
+              pricing {
+                price discount netPrice
+              }
+              imageUrl {
+                _id nameUrl
+              }
+              shipping {
+                weight heels shoeTip
+              }
+              active
+            }
+          }
+        `,
+    },
+    API_HEADER
+  );
+  const products = res.data.data.getProducts;
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      products,
+    },
+  };
 }
